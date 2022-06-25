3c17078651f0234dc4f15769748b1f8e9b05eb12
shubham pawar <shubham.vitthal.pawar@gmail.com>
2022-06-25 15:10:29 +0530

    Adding 2nd new file into the repo

---
+++ diff.java
@@ -0,0 +1,9 @@
+class diff
+{
+    public static void main(String[] args) {
+        int x=3;
+        int y=4;
+        int diffrence=x-y;
+        System.out.println("Diffrence:"+diffrence);
+    }
+}
